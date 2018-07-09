ea7764f2e34987646ed9257e84cf44b1139b65d1
karthikbalajikb <karthik.akb009@gmail.com>
2018-07-10 01:28:43 +0530

    add responsiveness and toggle side menu

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,24 +7,61 @@ import Header from '../components/header'
 import './index.scss'
 import favicon from '../../assets/logo.jpg'
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Blog' },
-        { name: 'keywords', content: 'Blog, Javascript, ES6, Learn, Guide, Personal,' },
-      ]}
-      link={[
-        { rel: 'shortcut icon', type: 'image/jpg', href: `${favicon}` }
-    ]}
-    />
-    <div className="kbts-site-container">
-      <SideMenu logo={data.headerImage.sizes}/>
-      <main className="site-main-content">{children()}</main>
+class Layout extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      sideMenu: 'closed'
+    }
+  }
+
+  toggleSideMenu = () => {
+    if(this.state.sideMenu === 'open'){
+      document.getElementsByClassName('kbts-site-container')[0].style['grid-template-columns'] = "auto";
+      this.setState({sideMenu: 'closed'});
+    }
+    else if(this.state.sideMenu === 'closed'){
+      document.getElementsByClassName('kbts-site-container')[0].style['grid-template-columns'] = "120px auto";
+      this.setState({sideMenu: 'open'});
+    }
+  }
+
+  componentDidMount(){
+    document.getElementById('kbts-sidemenu-btn').addEventListener('click',this.toggleSideMenu);
+  }
+
+  componentWillUnmount(){
+    document.getElementById('kbts-sidemenu-btn').removeEventListener('click',this.toggleSideMenu);
+  }
+
+  render(){
+    return (
+      <div>
+      <Helmet
+        title={this.props.data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Blog' },
+          {
+            name: 'keywords',
+            content: 'Blog, Javascript, ES6, Learn, Guide, Personal,',
+          },
+        ]}
+        link={[{ rel: 'shortcut icon', type: 'image/jpg', href: `${favicon}` }]}
+      />
+      <div className="kbts-site-container">
+        <SideMenu logo={this.props.data.headerImage.sizes} />
+        <section className="kbts-site-main">
+          <header className="kbts-site-header">
+            <a id="kbts-sidemenu-btn" className="kbts-site-header-menu" />
+            <a className="kbts-site-header-text">KB TECHSPACE</a>
+          </header>
+          <main className="site-main-content">{this.props.children()}</main>
+        </section>
+      </div>
     </div>
-  </div>
-)
+    )
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.func,
@@ -41,7 +78,7 @@ export const query = graphql`
     }
 
     headerImage: imageSharp(id: { regex: "/logo/" }) {
-      sizes(maxWidth: 1240 ) {
+      sizes(maxWidth: 1240) {
         ...GatsbyImageSharpSizes
       }
     }
