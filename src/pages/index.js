f8c962245e58f502ed4017015b8ece399378ae8b
karthikbalajikb <karthik.akb009@gmail.com>
2018-07-12 02:32:01 +0530

    added theme-color meta tag meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Img from "gatsby-image";
+import Img from 'gatsby-image'
+import NameCard from '../components/NameCard/name-card'
 
-const IndexPage = ({data}) => (
+const IndexPage = ({ data }) => (
   <div>
-   <Img sizes={data.headerImage.sizes} className="header-image"/>
+    <Img sizes={data.headerImage.sizes} className="header-image" />
+    <NameCard text_1="Hi, I am" text_2="Karthik Balaji" text_3="I am a Javascript enthusiast, I am a Blogger" />
   </div>
 )
 
@@ -13,7 +15,7 @@ export default IndexPage
 export const query = graphql`
   query MainWallPaperQuery {
     headerImage: imageSharp(id: { regex: "/tech-wallpapers/" }) {
-      sizes(maxWidth: 1240 ) {
+      sizes(maxWidth: 1240) {
         ...GatsbyImageSharpSizes
       }
     }
