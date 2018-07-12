e299867398197dc681a5873dfd7365baa8acfa58
karthikbalajikb <karthik.akb009@gmail.com>
2018-07-13 00:58:20 +0530

    added remark plugin [ci skip]

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,14 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/docs/markdown`,
+        name: "markdown-pages",
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
