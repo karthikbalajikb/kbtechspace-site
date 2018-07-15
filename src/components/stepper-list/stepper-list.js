96273113236e62931808a7d5edf8d4403f175b96
karthikbalajikb <karthik.akb009@gmail.com>
2018-07-16 01:35:16 +0530

    stepper component css added

--- src/components/stepper-list/stepper-list.js
+++ src/components/stepper-list/stepper-list.js
@@ -3,14 +3,14 @@ import './stepper-list.scss';
 
 const StepperList = props => (
   <article className="kbts-stepper-list">
-    <h3>{props.title}</h3>
+    <h3 className="kbts-stepper-list-header-title">{props.title}</h3>
     <p><i className="kbts-stepper-list-icon fas fa-laptop"></i></p>
     {props.jobs.map(d => (
-      <div>
-        <p>{d.duration}</p>
-        <p>{d.role}</p>
-        <p>{d.company}</p>
-        <p>{d.description}</p>
+      <div className="kbts-stepper-list-item">
+        <p className="kbts-stepper-list-item-label">{d.duration}</p>
+        <p className="kbts-stepper-list-item-title">{d.role}</p>
+        <p className="kbts-stepper-list-item-subtext">{d.company}</p>
+        <p className="kbts-stepper-list-item-description">{d.description}</p>
       </div>
     ))}
   </article>
