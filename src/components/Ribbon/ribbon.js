2ca914de2de8e80500d84b5ca0d3c5d0b0853b4f
karthikbalajikb <karthik.akb009@gmail.com>
2018-07-23 01:02:33 +0530

    ribbon component

---
+++ src/components/Ribbon/ribbon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './ribbon.scss';
+
+const Ribbon = () => (
+    <h3 className="kbts-ribbon-header"><i className="kbts-ribbon-header-text">coding skills</i></h3>
+)
+
+export default Ribbon
