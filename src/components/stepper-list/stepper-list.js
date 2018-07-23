741a66bc1c997e529c098b958ddc8ac9ac3f0ff7
karthikbalajikb <karthik.akb009@gmail.com>
2018-07-24 00:51:02 +0530

    pass icon and iscurrenr as prop

--- src/components/stepper-list/stepper-list.js
+++ src/components/stepper-list/stepper-list.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './stepper-list.scss';
 
+function markAsCurrent(current = false){
+  return current ? 'kbts-stepper-list-item--current' : ''
+}
+
 const StepperList = props => (
   <article className="kbts-stepper-list">
     <h3 className="kbts-stepper-list-header-title">{props.title}</h3>
-    <p><i className="kbts-stepper-list-icon fas fa-laptop"></i></p>
+    <p><i className={`kbts-stepper-list-icon fas ${props.icon}`}></i></p>
     {props.jobs.map(d => (
-      <div className="kbts-stepper-list-item">
+      <div className={`kbts-stepper-list-item `+markAsCurrent(d.markCurrent)}>
         <p className="kbts-stepper-list-item-label">{d.duration}</p>
         <p className="kbts-stepper-list-item-title">{d.role}</p>
         <p className="kbts-stepper-list-item-subtext">{d.company}</p>
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,9 +4,9 @@ import Link from 'gatsby-link'
 import PageHeader from '../components/page-header/page-header'
 import StepperList from '../components/stepper-list/stepper-list'
 import SliderList from '../components/SliderList/slider-list'
-import Ribbon from '../components/Ribbon/ribbon' 
+import Ribbon from '../components/Ribbon/ribbon'
 
-import '../pageCSS/resume.scss';
+import '../pageCSS/resume.scss'
 
 const jobs = [
   {
@@ -14,6 +14,7 @@ const jobs = [
     role: 'Senior System Engineer',
     company: 'Infosys',
     description: 'Passion alone drive me',
+    markCurrent: true,
   },
   {
     duration: '2016 - 2017',
@@ -31,19 +32,50 @@ const jobs = [
   },
 ]
 
+const eduction = [
+  {
+    duration: '2011 - 2015',
+    role: 'B.E in Computer science',
+    company: 'panimalar engineering college',
+    description: 'Graduated in first class with 8.2 CGPA',
+  },
+]
+
 const Resume = () => (
   <section className="kbts-page-resume">
     <PageHeader title="resume" logo="fas fa-id-card-alt" />
     <div className="kbts-page-resume-content">
-      <StepperList title="WORK HISTORY" jobs={jobs} />
-
+      <StepperList title="WORK HISTORY" icon="fa-laptop" jobs={jobs} />
       <article className="kbts-page-resume-skills">
-        <Ribbon />
+        <Ribbon title="coding skills" />
         <SliderList text="HTML5" value="100" />
         <SliderList text="CSS3" value="90" />
         <SliderList text="React" value="90" />
-        <SliderList text="Angular" value="70" />
+        <SliderList text="Angular" value="90" />
+        <SliderList text="NodeJS" value="70" />
+
+        <Ribbon title="Standards & Patterns" />
+        <SliderList text="ES6" value="100" />
+        <SliderList text="REDUX" value="80" />
+
+        <Ribbon title="Cloud" />
+        <SliderList text="Amazon web services" value="70" />
+
+        <Ribbon title="Build tools" />
+        <SliderList text="Webpack" value="100" />
+        <SliderList text="Rollup" value="80" />
+
+        <Ribbon title="CMS" />
+        <SliderList text="Wordpress" value="80" />
+        <SliderList text="WooCommerce" value="90" />
+
+        <Ribbon title="External Services" />
+        <SliderList text="IBM Watson" value="80" />
+        <SliderList text="Google API" value="70" />
+        <SliderList text="Facebook API" value="80" />
+        <SliderList text="Twilio API" value="70" />
       </article>
+      <StepperList title="EDUCATION" icon="fa-graduation-cap" jobs={eduction} />
     </div>
   </section>
 )
