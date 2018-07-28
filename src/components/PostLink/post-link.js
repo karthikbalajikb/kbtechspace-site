5d331a71c0ac3f153a0e4a5fc3b22fd4777fc7f2
karthikbalajikb <karthik.akb009@gmail.com>
2018-07-29 01:24:59 +0530

    added SCSS [ci skip]

--- src/components/PostLink/post-link.js
+++ src/components/PostLink/post-link.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Link from "gatsby-link";
 
+import './post-link.scss'
+
 const PostLink = ({ post }) => (
   <div>
-    <Link to={post.frontmatter.path}>
+    <Link to={post.frontmatter.path} className="kbts-post-link">
       {post.frontmatter.title} ({post.frontmatter.date})
     </Link>
   </div>
