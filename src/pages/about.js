b1ee267f1c20d7e05092ea7190d3da60accdc9c2
karthikbalajikb <karthik.akb009@gmail.com>
2018-07-31 01:43:23 +0530

    added service card to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,33 @@
 import React from 'react'
 
-
 import PageHeader from '../components/page-header/page-header'
+import Ribbon from '../components/Ribbon/ribbon'
+import ServiceCard from '../components/ServiceCard/service-card'
+import '../pageCSS/about.scss'
 
-const About = () => (
-    <section>
-        <PageHeader title="about me" logo="fas fa-user-secret" />
-        <p>Call me as KB</p>
+const About = ({ data }) => (
+  <section className="kbts-page-about">
+    <PageHeader title="about me" logo="fas fa-user-secret" />
+    <div className="kbts-page-about-ribbon">
+      <Ribbon title="Services" />
+    </div>
+    <section className="kbts-page-about-service">
+      {data.allAboutJson.edges.map(d => <ServiceCard data={d.node} />)}
     </section>
+  </section>
 )
 
 export default About
+
+export const query = graphql`
+  query serviceCardData {
+    allAboutJson {
+      edges {
+        node {
+          title
+          description
+        }
+      }
+    }
+  }
+`
