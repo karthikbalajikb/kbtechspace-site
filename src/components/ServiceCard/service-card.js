230f9601d1457416cc10cfde66e695cbd06ef293
karthikbalajikb <karthik.akb009@gmail.com>
2018-08-04 21:08:40 +0530

    made icon dynamic for service card

--- src/components/ServiceCard/service-card.js
+++ src/components/ServiceCard/service-card.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import logo from '../../../assets/icons/icon-02.png'
+import mobileLogo from '../../../assets/icons/mobile_in_hand.png';
+
 import './service-card.scss'
 
 const ServiceCard = ({data}) => (
   <article className="kbts-servicecard">
     <div className="kbts-servicecard-img">
-      <img src={logo} />
+      <img src={require(`../../../assets/icons/${data.icon}`)} />
     </div>
     <div className="kbts-servicecard-title">{data.title}</div>
     <div className="kbts-servicecard-desc">
