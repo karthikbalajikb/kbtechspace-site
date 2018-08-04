0716ad8d499d605445252d5ad1eefd63448a764d
karthikbalajikb <karthik.akb009@gmail.com>
2018-08-04 21:14:27 +0530

    removed unused import

--- src/components/ServiceCard/service-card.js
+++ src/components/ServiceCard/service-card.js
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import logo from '../../../assets/icons/icon-02.png'
-import mobileLogo from '../../../assets/icons/mobile_in_hand.png';
-
 import './service-card.scss'
 
 const ServiceCard = ({data}) => (
