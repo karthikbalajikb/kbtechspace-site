92ca9b44d967b09dfbdaeca3d6bb0a1ddc788c4b
karthikbalajikb <karthik.akb009@gmail.com>
2018-09-24 02:18:08 +0530

    adding json schema script  and open graph meta tag

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,15 +1,48 @@
-import React from "react";
-import Helmet from 'react-helmet';
-import './blog-template.scss';
+import React from 'react'
+import Helmet from 'react-helmet'
+import './blog-template.scss'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
-  const { markdownRemark } = data; // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark;
+  const { markdownRemark } = data // data.markdownRemark holds our post data
+  const { frontmatter, html } = markdownRemark
+  const getJSONLD = () => ({
+    '@context': 'http://schema.org',
+    '@type': 'Article',
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': `https://kbtechspace.com${frontmatter.path}`,
+    },
+    headline: `${frontmatter.title}`,
+    image: {
+      '@type': 'ImageObject',
+      url: `${frontmatter.banner}`,
+      height: 630,
+      width: 1200,
+    },
+    datePublished: `${frontmatter.date}`,
+    dateModified: `${frontmatter.date}`,
+    author: {
+      '@type': 'Person',
+      name: 'Karthik Balaji',
+    },
+    publisher: {
+      '@type': 'Organization',
+      name: 'KB TechSpace',
+      logo: {
+        '@type': 'ImageObject',
+        url:
+          'https://user-images.githubusercontent.com/8594076/45932661-210beb00-bf9d-11e8-892c-192e9bba6750.jpg',
+        width: 257,
+        height: 60,
+      },
+    },
+    description: `${frontmatter.title}`,
+  })
   return (
     <div>
-       <Helmet
+      <Helmet
         title={`${frontmatter.title} | Karthik Balaji`}
         meta={[
           { name: 'description', content: `${frontmatter.title}` },
@@ -19,23 +52,61 @@ export default function Template({
           },
           {
             name: 'theme-color',
-            content: '#540a2f'
-          }
+            content: '#540a2f',
+          },
+          {
+            property: 'og:site_name',
+            content: 'Kb TechSpace',
+          },
+          {
+            property: 'og:type',
+            content: 'article',
+          },
+          {
+            property: 'og:url',
+            content: `https://kbtechspace.com${frontmatter.path}`,
+          },
+          {
+            property: 'og:title',
+            content: `${frontmatter.title}`,
+          },
+          {
+            property: 'og:image',
+            content: `${frontmatter.banner}`,
+          },
+          {
+            property: 'og:description',
+            content: `${frontmatter.title}`,
+          },
+          {
+            property: 'og:locale',
+            content: `en_US`,
+          },
+        ]}
+        script={[
+          {
+            type: 'application/ld+json',
+            innerHTML: `${JSON.stringify(getJSONLD())}`,
+          },
         ]}
       />
-    <div className="blog__post__container">
-      <div className="blog__post">
-        <h1 className="blog__post__title">{frontmatter.title}</h1>
-        <p className="blog__post__date">{frontmatter.date}</p>
-        <div className="blog__post__tags">{frontmatter.tags.map(d => <p className="blog__post__tags__text">{d}</p>)}</div>
-        <div
-          className="blog__post__content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+      <div className="blog__post__container">
+        <div className="blog__post">
+          <h1 className="blog__post__title">{frontmatter.title}</h1>
+          <p className="blog__post__date">{frontmatter.date}</p>
+          <div className="blog__post__tags">
+            {frontmatter.tags.map(d => (
+              <p className="blog__post__tags__text">{d}</p>
+            ))}
+          </div>
+          <div
+            className="blog__post__content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
     </div>
-    </div>
-  );
+  )
 }
 
 export const pageQuery = graphql`
@@ -50,4 +121,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
