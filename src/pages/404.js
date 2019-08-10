24458eb8fe532f7c78c5f73754e9f204806914a8
karthikbalajikb <karthik.akb009@gmail.com>
2019-08-11 02:44:11 +0530

    updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,31 @@
 import React from 'react'
+import Img from 'gatsby-image'
+import styled from 'styled-components'
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+const NotFoundPage = ({ data }) => (
+  <Container>
+    <Img sizes={data.notFoundImage.sizes} />
+    <p style={{ marginTop: 20 }}>
+      If you liked to go in a right way ... Click the link {' '}
+      <a href="https://kbtechspace.com">kbtechspace</a>
+    </p>
+  </Container>
 )
 
 export default NotFoundPage
+
+export const query = graphql`
+  query NotFoundQuery {
+    notFoundImage: imageSharp(fluid: { originalName: { regex: "/404fuck/" } }) {
+      sizes(maxWidth: 1240) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+  }
+`
+
+const Container = styled.div`
+  width: 50%;
+  height: 50%;
+  margin: auto;
+`
