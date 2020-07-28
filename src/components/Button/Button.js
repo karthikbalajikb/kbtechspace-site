b038c442bc9509a60536da46e2bcd9839eae45bd
Karthik Balaji <karthik.akb009@gmail.com>
2020-07-28 23:30:57 +0530

    feat(Button): support for width height prop

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,25 +2,41 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
-const Button = ({ label, onClick }) => (
-  <StyledButton onClick={onClick}>{label}</StyledButton>
+const Button = ({ label, width, height, onClick }) => (
+  <StyledButton width={width} height={height} onClick={onClick}>
+    {label}
+  </StyledButton>
 )
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
   onClick: PropTypes.func.isRequired,
 }
+
+Button.defaultProps = {
+  label: PropTypes.string.isRequired,
+  width: '120px',
+  height: '37px',
+  onClick: PropTypes.func.isRequired,
+}
+
 export default Button
 
 const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border: none;
   outline: none;
   font-size: 13px;
-  width: 120px;
-  height: 37px;
+  width: ${({ width }) => `${width}`};
+  height: ${({ height }) => `${height}`};
   cursor: pointer;
   border-radius: ${({ theme }) => `${theme.borderRadius.rounded}`};
-  background: ${({ theme }) =>  `linear-gradient(145deg, ${theme.hoverSurface.light} , ${theme.hoverSurface.main})` };
+  background: ${({ theme }) =>
+    `linear-gradient(145deg, ${theme.hoverSurface.light} , ${theme.hoverSurface.main})`};
   box-shadow: ${({ theme }) => `${theme.boxShadow.smActive}`};
   color: ${({ theme }) => `${theme.onSurface.main}`};
 
@@ -29,7 +45,8 @@ const StyledButton = styled.button`
   }
 
   :active {
-    background: ${({ theme }) =>  `linear-gradient(20deg, ${theme.hoverSurface.light} , ${theme.hoverSurface.main})` };
+    background: ${({ theme }) =>
+      `linear-gradient(20deg, ${theme.hoverSurface.light} , ${theme.hoverSurface.main})`};
     box-shadow: ${({ theme }) => `${theme.boxShadow.smActive}`};
   }
 `
